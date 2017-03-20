b995f2abcdc1417301eafc9dd82e249a444d5577
Michael Stanaland <mstanaland@mac.com>
2017-03-20 14:22:22 -0400

    Cleanup

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@
  *  value: the parsed entry (no spaces, hyphens or other chars)
  *  cleanedValue: the entry as the user typed it up to the first offending char
  *  originalValue: the entry as the user supplied it
- *  
+ *
  * @param  {string} entry the value to test. Usually the value of an input
  * @return {object}       returns an object
  */
@@ -17,7 +17,7 @@ function parseCode(entry) {
   var isValid = true;
   var number = /[0-9]/;
   var separator = /(?:-| )/;
-  
+
   // Check if entry is a string and not empty
   if (typeof entry === 'string' && entry.length) {
     // Loop over input text and check if each char is a digit or separator
@@ -25,29 +25,27 @@ function parseCode(entry) {
       if (number.test(entry[i])) {
         parsedEntry += entry[i];
         cleanedEntry += entry[i];
-      } 
-      else if (separator.test(entry[i])) {
+      } else if (separator.test(entry[i])) {
         cleanedEntry += entry[i];
-      } 
-      else {
+      } else {
         // char is an illegal character so stop checking
         isValid = false;
         break;
       }
     }
-    
+
     // If there were no illegal characters and there's 8 numbers then it's good
     if (isValid && parsedEntry.length === 8) {
       isValid = true;
     } else {
       isValid = false;
     }
-    
+
     return {
       isValid: isValid,
       value: parsedEntry,
       cleanedValue: cleanedEntry,
-      originalValue: entry
+      originalValue: entry,
     };
   }
 }
@@ -61,7 +59,7 @@ function parseCode(entry) {
  *  value: the parsed entry (no spaces, hyphens or other chars)
  *  cleanedValue: the entry as the user typed it up to the first offending char
  *  originalValue: the entry as the user supplied it
- *  
+ *
  * @param  {string} entry the value to test. Usually the value of an input
  * @return {object}       returns an object
  */
@@ -72,54 +70,50 @@ function parsePhone(entry) {
   var number = /[0-9]/;
   var separator = /(?: |-|\+|\)|\(|\.|\*|\#)/;
   var length;
-  
+
   if (typeof entry === 'string' && entry.length) {
     for (var i = 0; i < entry.length; i += 1) {
       if (number.test(entry[i])) {
         parsedEntry += entry[i];
         cleanedEntry += entry[i];
-      } 
-      else if (separator.test(entry[i])) {
+      } else if (separator.test(entry[i])) {
         cleanedEntry += entry[i];
-      } 
-      else {
+      } else {
         isValid = false;
         break;
       }
     }
-    
+
     length = parsedEntry.length;
-    
+
     // 10 digits must be area code + 3 + 4
     // Area code can't start with a 0 or 1
     if (length === 10 && parseInt(parsedEntry[0]) > 1) {
       isValid = true;
-    }
-    // 11 digits must be 1 + area code + 3 + 4
-    // leading 1 is stripped out
-    else if (length === 11 && parsedEntry[0] === '1') {
+    } else if (length === 11 && parsedEntry[0] === '1') {
+      // 11 digits must be 1 + area code + 3 + 4
+      // leading 1 is stripped out
       parsedEntry = parsedEntry.substring(1);
       isValid = true;
     }
   }
-  
+
   return {
     isValid: isValid,
     value: parsedEntry,
     cleanedValue: cleanedEntry,
-    originalValue: entry
+    originalValue: entry,
   };
-  
 }
 
 // Test the code validation
 // ---------------------------
 $('#testCode').submit(function(e) {
   e.preventDefault();
-  
+
   var theConfCodeValue = $('#theConfCode').val();
   var code = parseCode(theConfCodeValue);
-  
+
   if (code.isValid) {
     console.log('Valid parsed code', code.value);
   } else {
@@ -132,13 +126,13 @@ $('#testCode').submit(function(e) {
 // ---------------------------
 $('#testPhone').submit(function(e) {
   e.preventDefault();
-  
+
   var thePhoneNumberValue = $('#thePhoneNumber').val();
   var phone = parsePhone(thePhoneNumberValue);
-  
+
   if (phone.isValid) {
     console.log('Valid parsed phone', phone.value);
   } else {
     alert('Invalid U.S. phone number. Enter your phone number with area code.');
   }
-});  
+});

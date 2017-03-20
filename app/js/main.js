7ff6023459b03bbc76a361c3eb0573f5dbbd436f
Michael Stanaland <mstanaland@mac.com>
2017-03-20 14:55:22 -0400

    Added additional formats to returned object

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,9 @@
  *  Returns an object with these properties:
  *  isValid : Bool, true if is valid
  *  value: the parsed entry (no spaces, hyphens or other chars)
+ *  formatted: the value with separating hyphen
+ *  codepart1: the first 4 digits
+ *  codepart2: the last 4 digits
  *  cleanedValue: the entry as the user typed it up to the first offending char
  *  originalValue: the entry as the user supplied it
  *
@@ -15,8 +18,12 @@ function parseCode(entry) {
   var cleanedEntry = '';
   var parsedEntry = '';
   var isValid = true;
+  var codeFormat = /^([0-9]{4})[- ]?([0-9]{4})$/;
   var number = /[0-9]/;
   var separator = /(?:-| )/;
+  var formatted;
+  var codepart1;
+  var codepart2;
 
   // Check if entry is a string and not empty
   if (typeof entry === 'string' && entry.length) {
@@ -37,6 +44,9 @@ function parseCode(entry) {
     // If there were no illegal characters and there's 8 numbers then it's good
     if (isValid && parsedEntry.length === 8) {
       isValid = true;
+      formatted = parsedEntry.replace(codeFormat, '$1-$2');
+      codepart1 = parsedEntry.replace(codeFormat, '$1');
+      codepart2 = parsedEntry.replace(codeFormat, '$2');
     } else {
       isValid = false;
     }
@@ -44,6 +54,9 @@ function parseCode(entry) {
     return {
       isValid: isValid,
       value: parsedEntry,
+      formatted: formatted,
+      codepart1: codepart1,
+      codepart2: codepart2,
       cleanedValue: cleanedEntry,
       originalValue: entry,
     };
@@ -69,7 +82,12 @@ function parsePhone(entry) {
   var isValid = false;
   var number = /[0-9]/;
   var separator = /(?: |-|\+|\)|\(|\.|\*|\#)/;
+  var phoneFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
   var length;
+  var formatted;
+  var areaCode;
+  var part1;
+  var part2;
 
   if (typeof entry === 'string' && entry.length) {
     for (var i = 0; i < entry.length; i += 1) {
@@ -95,12 +113,25 @@ function parsePhone(entry) {
       // leading 1 is stripped out
       parsedEntry = parsedEntry.substring(1);
       isValid = true;
+    } else {
+      isValid = false;
     }
+  }
+  
+  if (isValid) {
+    formatted = parsedEntry.replace(phoneFormat, '($1) $2-$3');
+    areaCode = parsedEntry.replace(phoneFormat, '$1');
+    part1 = parsedEntry.replace(phoneFormat, '$2');
+    part2 = parsedEntry.replace(phoneFormat, '$3');
   }
 
   return {
     isValid: isValid,
     value: parsedEntry,
+    formatted: formatted,
+    areaCode: areaCode,
+    part1: part1,
+    part2: part2,
     cleanedValue: cleanedEntry,
     originalValue: entry,
   };
@@ -115,7 +146,9 @@ $('#testCode').submit(function(e) {
   var code = parseCode(theConfCodeValue);
 
   if (code.isValid) {
-    console.log('Valid parsed code', code.value);
+    console.log(code.formatted + ' is a valid code');
+    console.log('Code part 1', code.codepart1);
+    console.log('Code part 2', code.codepart2);
   } else {
     alert('Invalid code. Enter the code that was emailed to you.');
     $('#theConfCode').val(code.cleanedValue);
@@ -131,7 +164,10 @@ $('#testPhone').submit(function(e) {
   var phone = parsePhone(thePhoneNumberValue);
 
   if (phone.isValid) {
-    console.log('Valid parsed phone', phone.value);
+    console.log(phone.formatted + ' is a valid U.S. phone number format');
+    console.log('Area code', phone.areaCode);
+    console.log('Part 1', phone.part1);
+    console.log('Part 2', phone.part2);
   } else {
     alert('Invalid U.S. phone number. Enter your phone number with area code.');
   }
